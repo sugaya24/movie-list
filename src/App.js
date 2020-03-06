e61badd6f4f7a660ab9da62d5c79756ee02910bb
YukiSugaya <sugaya324@gmail.com>
2020-03-06 01:18:12 -0800

    test

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
           data: data.results
         });
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err, 'catch something error'));
   };
 
   handleClick = (movie) => {
